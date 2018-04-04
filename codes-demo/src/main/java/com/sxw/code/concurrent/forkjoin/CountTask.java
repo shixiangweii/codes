da2ab4f4b77888c10329d8334c35aa3e3e23600b
shixiangweii <fengzaoxiangtai@sina.cn>
2018-04-04 23:26:29 +0800

    二分法计算,JUC,fork/join

---
+++ codes-demo/src/main/java/com/sxw/code/concurrent/forkjoin/CountTask.java
@@ -0,0 +1,65 @@
+package com.sxw.code.concurrent.forkjoin;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Description:
+ * User: shixiangweii
+ * Date: 2018-04-04
+ * Time: 23:00
+ */
+public class CountTask extends RecursiveTask<Integer> {
+
+    /**
+     * 任务分解的阙值
+     */
+    private static final int THRESHOLD = 2;
+
+    private int start;
+    private int end;
+
+    public CountTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+
+        boolean canCompute = (end - start) <= THRESHOLD;
+        if (canCompute) {
+            for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            int middle = (start + end) / 2;
+            CountTask leftTask = new CountTask(start, middle);
+            CountTask rightTask = new CountTask(middle + 1, end);
+            leftTask.fork();
+            rightTask.fork();
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+            sum = leftResult + rightResult;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask task = new CountTask(1, 4);
+        Future<Integer> result = forkJoinPool.submit(task);
+
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
