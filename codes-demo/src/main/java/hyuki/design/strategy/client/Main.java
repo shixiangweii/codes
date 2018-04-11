c67cceb962e3fbc50e9c781f30811f35098d3c77
shixiangweii <fengzaoxiangtai@sina.cn>
2018-04-11 09:03:50 +0800

    hyuki，策略模式

---
+++ codes-demo/src/main/java/hyuki/design/strategy/client/Main.java
@@ -0,0 +1,14 @@
+package hyuki.design.strategy.client;
+
+/**
+ * Description:
+ * User: shixiangweii
+ * Date: 2018-04-11
+ * Time: 8:29
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
