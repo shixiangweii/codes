809ae79a13646a1d6374176d787543068a250491
shixiangweii <fengzaoxiangtai@sina.cn>
2018-04-13 15:03:57 +0800

    责任链模式

---
+++ codes-demo/src/main/java/hyuki/design/chainresponsibility/Trouble.java
@@ -0,0 +1,18 @@
+package hyuki.design.chainresponsibility;
+
+/**
+ * Description:
+ * User: shixiangweii
+ * Date: 2018-04-11
+ * Time: 18:52
+ */
+public class Trouble {
+    private int number;
+
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+
+
+}
