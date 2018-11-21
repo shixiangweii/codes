a5ee6aaee85527ea8b79505c677cb1319a291c93
shixiangweii <fengzaoxiangtai@sina.cn>
2018-11-21 09:01:14 +0800

    分库分表配置测试

--- sharding_jdbc_projs_sharding_3/src/test/java/com/lyz/sharding/test/ShardingJdbcMybatisTest.java
+++ sharding_jdbc_projs_sharding_3/src/test/java/com/lyz/sharding/test/ShardingJdbcMybatisTest.java
@@ -85,6 +85,18 @@ public class ShardingJdbcMybatisTest {
         userService.transactionTestSucess();
     }
 
+    /**
+     * https://blog.csdn.net/yanyan19880509/article/details/78335935
+     * 1) 完全支持非跨库事务，例如：仅分表，或分库但是路由的结果在单库中
+     * 2) 完全支持因逻辑异常导致的跨库事务。例如：同一事务中，跨两个库更新。更新完毕后，抛出空指针，则两个库的内容都能回滚。
+     * 3) 不支持因网络、硬件异常导致的跨库事务。例如：同一事务中，跨两个库更新，更新完毕后、未提交之前，第一个库死机，则只有第二个库数据提交
+     *
+     * 下面的例子就验证了（2），其实，突然感觉，假设，数据库不出现物理错误，只是代码的逻辑异常，
+     * 那么其实，假设数据库，db1,db2,db3...dbn，其实事务成功就是：T1 && T2 && T3...&& Tn
+     * 就是n个事务的之间逻辑与，
+     * 然后任意一个Ti失败，则就都回滚
+     * 这么想的话，分库分表其实，感觉用sharding-jdbc就其实很简单了，似乎也就好理解了
+     */
     @Test(expected = IllegalAccessException.class)
     public void testTransactionTestFailure() throws IllegalAccessException {
         userService.transactionTestFailure();
