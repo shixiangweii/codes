cbe8c2386a91cf319602f26bd103c8222b0e84c7
shixiangweii <fengzaoxiangtai@sina.cn>
2018-12-09 20:13:33 +0800

    通用VO工具

---
+++ codes-demo/src/main/java/com/sxw/code/util/vo/ViewObjUtil.java
@@ -0,0 +1,26 @@
+package com.sxw.code.util.vo;
+
+import java.util.*;
+
+/**
+ * Description:
+ * User: shixiangweii
+ * Date: 2018-12-09
+ * Time: 20:05
+ *
+ * @author shixiangweii
+ */
+public class ViewObjUtil {
+
+    public static Map<String, Object> getOneMap(int size) {
+        return new HashMap<>(size <= 0 ? 16 : size);
+    }
+
+    public static List<Map<String, Object>> getList() {
+        return new LinkedList<>();
+    }
+
+    public static List<Map<String, Object>> getList(int size) {
+        return new ArrayList<>(size <= 0 ? 10 : size);
+    }
+}
