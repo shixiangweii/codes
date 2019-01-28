52c48096777d212b24ba0b2d7fef06778ce700a3
shixiangweii <fengzaoxiangtai@sina.cn>
2019-01-28 16:26:52 +0800

    数值转换

--- codes-demo/src/test/java/com/sxw/test/basejava/InitializationDemo.java
+++ codes-demo/src/test/java/com/sxw/test/basejava/InitializationDemo.java
@@ -10,6 +10,7 @@ package com.sxw.test.basejava;
  */
 public class InitializationDemo {
 
+    private int a;
 
     public static void main(String[] args) {
         // https://www.nowcoder.com/questionTerminal/036b784ff0dc4cc5b2e046dbf6b1113a
@@ -17,6 +18,9 @@ public class InitializationDemo {
         //String s;
         //System.out.println("s=" + s);
 
+        InitializationDemo initializationDemo = new InitializationDemo();
+        // 变量虽然为private，但因为main函数在该类中，所以即使private也仍可使用
+        System.out.println(initializationDemo.a);
 
 
     }
