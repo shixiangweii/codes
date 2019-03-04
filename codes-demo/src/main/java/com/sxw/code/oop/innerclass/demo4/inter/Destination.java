2dc9196bc148acdd05764dc8304464d17ff66184
shixiangweii <fengzaoxiangtai@sina.cn>
2019-03-04 16:37:03 +0800

    内部类-体现封装性，隐藏实现细节

---
+++ codes-demo/src/main/java/com/sxw/code/oop/innerclass/demo4/inter/Destination.java
@@ -0,0 +1,13 @@
+package com.sxw.code.oop.innerclass.demo4.inter;
+
+/**
+ * Description:
+ * User: shixiangweii
+ * Date: 2019-03-03
+ * Time: 23:27
+ *
+ * @author shixiangweii
+ */
+public interface Destination {
+    String readLabel();
+}
