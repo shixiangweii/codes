088aafbd127d89e7f3a4d39e5f9b4da3112074df
shixiangweii <fengzaoxiangtai@sina.cn>
2019-03-14 14:16:20 +0800

    big decimal double 测试

--- codes-demo/src/test/java/com/sxw/test/BigDecimalUtilTest.java
+++ codes-demo/src/test/java/com/sxw/test/BigDecimalUtilTest.java
@@ -3,6 +3,8 @@ package com.sxw.test;
 import com.sxw.code.util.BigDecimalUtil;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 /**
  * Description:
  * User: shixiangweii
@@ -18,5 +20,17 @@ public class BigDecimalUtilTest {
         BigDecimalUtil.div(10d, 3d);
         // 3.3333333333
         System.out.println(BigDecimalUtil.div(10d, 3d));
+    }
+
+    @Test
+    public void testDouble() {
+        System.out.println(0.2 + 0.1);
+        System.out.println(0.3 - 0.1);
+        System.out.println(0.2 * 0.1);
+        System.out.println(0.3 / 0.1);
+
+        double v = new BigDecimal("0.2").add(new BigDecimal(0.1)).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
+        System.out.println(v);
+        System.out.println(String.format("%.3f", v));
     }
 }
