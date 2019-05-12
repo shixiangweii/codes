25b54e1f50ed4b74ab4d06ea1efd67f8762c6503
shixiangweii <fengzaoxiangtai@sina.cn>
2019-05-12 12:20:03 +0800

    面试题整理

---
+++ codes-demo/src/main/java/com/sxw/code/sqlmaker/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * @author shixiangweii
+ * 脚本导入数据
+ * create table temp_del_comment as select
+ * insert into wbcms.tb_yyb_app_b_integral_detail select
+ * UPDATE wbcms.tp_zx_doctor d INNER JOIN
+ *
+ * 难点
+ * 图片上传
+ * excel数据导入
+ *
+ * 对应关系
+ *   主记录ID: activity.id, activity_img.activity_id
+ *   图片名字: activity_img.img
+ *
+ * 检查重复
+ *
+ */
+package com.sxw.code.sqlmaker;
---
+++ codes-demo/src/test/java/com/sxw/PoiTest.java
@@ -0,0 +1,13 @@
+package com.sxw;
+
+/**
+ * Description:
+ * User: shixiangweii
+ * Date: 2019-05-06
+ * Time: 8:23
+ *
+ * @author shixiangweii
+ */
+public class PoiTest {
+
+}
