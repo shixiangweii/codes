0054fec361a03dc07b65bb7fe0cf5ca9fa421d0e
shixiangweii <fengzaoxiangtai@sina.cn>
2019-07-13 18:39:17 +0800

    提交修改

--- codesdemo1-eureka/src/main/java/com/sxw/springcloud/App.java
+++ codesdemo1-eureka/src/main/java/com/sxw/springcloud/App.java
@@ -1,10 +1,21 @@
 package com.sxw.springcloud;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Hello world!
+ *
+ * @author shixiangweii
+ *
+ * java -jar eureka-server.jar --spring.profiles.active=peer1
+ * java -jar eureka-server.jar --spring.profiles.active=peer2
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new SpringApplicationBuilder((App.class)).web(true).run(args);
     }
 }
