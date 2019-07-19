5370c8675688db2af27f7e9b586cd82e400bad57
shixiangweii <fengzaoxiangtai@sina.cn>
2019-07-19 16:26:36 +0800

    api gateway

---
+++ api-gateway/src/main/java/com/sxw/springcloud/App.java
@@ -0,0 +1,19 @@
+package com.sxw.springcloud;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ * @author shixiangweii
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(App.class).web(true).run(args);
+    }
+}
