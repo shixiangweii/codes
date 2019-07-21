1649b6c938f53e2a460276c05519d773a38f4b57
shixiangweii <fengzaoxiangtai@sina.cn>
2019-07-21 16:22:12 +0800

    spring cloud config

---
+++ config-client/src/main/java/com/sxw/springcloud/TestController.java
@@ -0,0 +1,8 @@
+package com.sxw.springcloud;
+
+/**
+ * @author shixiangweii
+ * @date 2019/7/21 13:28
+ */
+public class TestController {
+}
---
+++ config-client/src/test/java/com/sxw/springcloud/AppTest.java
@@ -0,0 +1,5 @@
+package com.sxw.springcloud;
+
+
+public class AppTest {
+}
