31013785b5595da8853773be4f780e1549afd38a
shixiangweii <fengzaoxiangtai@sina.cn>
2020-02-02 13:09:12 +0800

    bean生命周期测试

--- codes-demo/src/main/java/com/sxw/spring/bean/lifecycle/TestBeanLife.java
+++ codes-demo/src/main/java/com/sxw/spring/bean/lifecycle/TestBeanLife.java
@@ -10,6 +10,26 @@ public class TestBeanLife {
     /**
      * com/sxw/spring/bean/lifecycle/beans.xml
      *
+     * 1)	Constructor Student()
+     *
+     * 2)	setter setStudentName
+     *
+     * 3)	BeanNameAware.setBeanName: myBean
+     *
+     * 4)	BeanFactoryAware.setBeanFactory
+     *
+     * 5)	ApplicationContextAware.setApplicationContext （注入上下文，使用例如：SpringUtils）
+     *
+     * 6)	BeanPostProcessor.postProcessBeforeInitialization
+     *
+     * 7)	    InitializingBean.afterPropertiesSet
+     *
+     * 8)	    init-method: myInit
+     *
+     * 9)	BeanPostProcessor.postProcessAfterInitialization（后置处理器，starter，绑定配置关系都是利用这个，超级有用）
+     *
+     * 10)	bean method printInfo()
+     *
      * @param args args
      */
     public static void main(String[] args) {
